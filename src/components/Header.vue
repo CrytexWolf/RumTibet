ec405b8ed35d7861fd41dee86c6afc63fdae8f82
CrytexWolf <71540922+CrytexWolf@users.noreply.github.com>
2024-11-11 04:19:58 +0500

    modified:   src/App.vue 	new file:   src/components/Footer.vue 	new file:   src/components/Header.vue 	modified:   src/style.css

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <div class="container">
+            <img src="/logo.svg" alt="">
+            <nav class="header__nav">
+                <a class="header__nav--item" href="#">Главная</a>
+                <a class="header__nav--item" href="#">Про гида</a>
+                <a class="header__nav--item" href="#">Программа тура</a>
+                <a class="header__nav--item" href="#">Стоимость</a>
+                <a class="header__nav--item" href="#">Блог</a>
+                <a class="header__nav--item" href="#">Контакты</a>
+                <a class="header__nav--item" href="#">Консультация</a>
+            </nav>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    
+</style>
