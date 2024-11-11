01bbb90e354ae89d94fe9d1b8cde8ae72b0a4ec9
CrytexWolf <71540922+CrytexWolf@users.noreply.github.com>
2024-11-11 16:12:54 +0500

    modified:   src/components/Header.vue 	new file:   src/components/UI/index.js 	modified:   src/main.js 	modified:   src/style.css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,18 @@
 <template>
-    <header>
+    <header class="header">
         <div class="container">
-            <img src="/logo.svg" alt="">
-            <nav class="header__nav">
-                <a class="header__nav--item" href="#">Главная</a>
-                <a class="header__nav--item" href="#">Про гида</a>
-                <a class="header__nav--item" href="#">Программа тура</a>
-                <a class="header__nav--item" href="#">Стоимость</a>
-                <a class="header__nav--item" href="#">Блог</a>
-                <a class="header__nav--item" href="#">Контакты</a>
-                <a class="header__nav--item" href="#">Консультация</a>
-            </nav>
+            <section class="header__inner">
+                <img src="/logo.svg" alt="">
+                <nav class="header__nav">
+                    <a class="header__nav--item" href="#">Главная</a>
+                    <a class="header__nav--item" href="#">Про гида</a>
+                    <a class="header__nav--item" href="#">Программа тура</a>
+                    <a class="header__nav--item" href="#">Стоимость</a>
+                    <a class="header__nav--item" href="#">Блог</a>
+                    <a class="header__nav--item" href="#">Контакты</a>
+                    <button class="header__nav--btn">Консультация</button>
+                </nav>
+            </section>
         </div>
     </header>
 </template>
@@ -20,5 +22,21 @@ export default {
 }
 </script>
 <style scoped>
-    
+
+    .header__inner {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .header__nav--item {
+        margin-right: 24px;
+        font-size: clamp(14px, 5vh, 18px);
+        color: #FDFDFD;
+    }
+
+    .header__nav--item:last-child {
+        margin: 0;
+    }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
+import components from './components/UI'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+components.forEach(component => {
+    app.component(component.name, component)
+})
