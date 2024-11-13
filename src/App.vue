820e823486c082a107154764d66df76bda15ce7a
CrytexWolf <71540922+CrytexWolf@users.noreply.github.com>
2024-11-13 14:33:21 +0500

    modified:   src/App.vue 	modified:   src/components/Footer.vue 	modified:   src/components/Header.vue 	modified:   src/style.css

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,103 @@
 
 <template> 
     <Header />
-        <main></main>
+        <main class="main">
+            <section class="intro">
+                <div class="container">
+                    <div class="intro__inner">
+                        <h1 class="intro__title">Насладись прогулкой в горах
+                            с командой единомышленников</h1>
+                        <form class="intro__from">
+                            <section class="intro__input--block">
+                                <select class="intro__input" name="" id="">
+                                    <option disabled value="">Локация для тура</option>
+                                </select>
+                                <span class="intro__subtitle">выберите из списка</span>
+                            </section>
+                            <section class="intro__input--block">
+                                <input class="intro__input" type="text" placeholder="Дата похода">
+                                <span class="intro__subtitle">укажите диапазон</span>
+                            </section>
+                            <section class="intro__input--block">
+                                <select class="intro__input" name="" id="">
+                                    <option disabled value="">Участники</option>
+                                </select>
+                                <span class="intro__subtitle">минимум 4 человека</span>
+                            </section>
+                            <a  class="intro__btn" href="#">Найти программу</a>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </main>
     <Footer />
 </template>
 
 <style scoped>
+    .main {
+        height: 100%;
+    }
+    .intro {
+        background-image: url('./assets/img/bg-intro.png');
+        background-size: cover;
+    }
+
+    .intro__inner {
+        padding-top: 551px;
+        padding-bottom: 82px;
+    }
+
+    .intro__title {
+        font-family: 'NextArt heavy';
+        font-size: 50px;
+        letter-spacing: 0.02em;
+        margin-bottom: 41px;
+    }
+
+    .intro__from {
+        display: flex;
+        justify-content: space-between;
+        background-color: rgb(255, 255, 255, 0.2);
+        padding: 40px;
+        border-radius: 10px;
+    }
+
+    .intro__input--block {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .intro__input {
+        width: 270px;
+        height: 52px;
+
+        border-radius: var(--border-10);
+        background-color: rgb(255, 255, 255, 0.2);
+        border: 1px solid #e1e3e4;
+        color: #FDFDFD;
+        margin-bottom: 8px;
+        padding: 14px 16px;
+    }
+
+    .input__intro::placeholder,
+    ::-webkit-input-placeholder,
+    option {
+        color: #FDFDFD;
+        font-weight: 700;
+        font-size: 16px;
+    }
+
+    .intro__btn {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 213px;
+        height: 52px;
+        color: #1A3E3E;
+        font-size: 16px;
+        font-weight: 700;
+        background-color: #FDFDFD;
+        border-radius: var(--border-10);
+    }
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <header class="header">
         <div class="container">
             <section class="header__inner">
-                <img src="/logo.svg" alt="">
+                <img src="/logo.svg" alt="РУМТИБЕТ">
                 <nav class="header__nav">
                     <a class="header__nav--item" href="#">Главная</a>
                     <a class="header__nav--item" href="#">Про гида</a>
@@ -22,6 +22,13 @@ export default {
 }
 </script>
 <style scoped>
+    .header {
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 72px;
+    }
 
     .header__inner {
         display: flex;
@@ -29,14 +36,28 @@ export default {
         justify-content: space-between;
     }
 
+    .header__nav {
+        display: flex;
+        align-items: center;
+    }
+
     .header__nav--item {
         margin-right: 24px;
         font-size: clamp(14px, 5vw, 18px);
-        color: #FDFDFD;
     }
 
     .header__nav--item:last-child {
         margin: 0;
+    }
+
+    .header__nav--btn {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: var(--border-10);
+        width: 157px;
+        height: 44px;
+        background-color: #1A3E3E;
     }
 
 </style>
